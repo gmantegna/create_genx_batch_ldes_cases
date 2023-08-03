b4bc26c10c60ac1658ae6bdd1e8229667673469d
Gabriel A. Mantegna <gm1710@della8.princeton.edu>
2023-08-02 22:12:16 -0400

    updates

--- create_batch_cases.py
+++ create_batch_cases.py
@@ -11,7 +11,7 @@ import create_vrestor_inputs
 # inputs
 template_path = Path("/home/gm1710/create_genx_batch_ldes_cases/case_runner_template")
 julia_path = Path("/usr/licensed/julia/1.8.2/bin/julia")
-destination_path = Path("/scratch/gpfs/gm1710/GenX_cases/LDES_072023_const_hours")
+destination_path = Path("/scratch/gpfs/gm1710/GenX_cases/LDES_072023_extra_sensitivities_all_zones")
 rep_period_lengths = [24,72,168,336,8760]
 num_rep_periods = [5,15,30,45,52,75,100]
 ldes_proportions = { # how total LDES is allocated to each meta region (fractions are fraction of total nationwide peak load in load data) 
@@ -33,7 +33,7 @@ region_to_zone_map = {
         "WECC": 3,
 }
 advnuclear_cost_base = 450000 # $/MW-yr including FOM-- with regional cost multiplier = 1
-run_colocated = True
+run_colocated = False
 default_num_zones=12
 run_default_num_zones_only=False
 zerocarbon_fuel_cost_base = 20 # $/mmbtu
@@ -245,49 +245,49 @@ for path in pg_output_paths:
 
         replacements = pd.DataFrame()
 
-        # base case
-        replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
-        if case == "no_vrestor" and num_zones == default_num_zones:
-            # run constant hours experiment
-            for zerocarbonct_max_cap in [-1,0]:
-                for num_periods,length in [(140,24),(70,48),(35,96),(28,120),(20,168),(10,336),(70,24),(35,48),(14,120),(10,168),(5,336),(50,24),(25,48),(10,120),(5,240),(35,24),(7,120),(5,168)]:
-                    replacements = make_replacements_df(replacements,[length],[num_periods],region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=zerocarbonct_max_cap,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-
-            # zerocarbon CT cost 25% higher
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base*1.25,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-
-            # zerocarbon CT cost 25% lower
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base*0.75,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-            
-            # zerocarbon CT fuel cost 25% higher
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base*1.25)
-
-            # zerocarbon CT fuel cost 25% lower
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base*0.75)
-            
+#        # base case
+#        replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#        
+#        if case == "no_vrestor" and num_zones == default_num_zones:
+#            # run constant hours experiment
+#            for zerocarbonct_max_cap in [-1,0]:
+#                for num_periods,length in [(140,24),(70,48),(35,96),(28,120),(20,168),(10,336),(70,24),(35,48),(14,120),(10,168),(5,336),(50,24),(25,48),(10,120),(5,240),(35,24),(7,120),(5,168)]:
+#                    replacements = make_replacements_df(replacements,[length],[num_periods],region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=zerocarbonct_max_cap,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#
+#            # zerocarbon CT cost 25% higher
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base*1.25,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#
+#            # zerocarbon CT cost 25% lower
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base*0.75,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#            
+#            # zerocarbon CT fuel cost 25% higher
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base*1.25)
+#
+#            # zerocarbon CT fuel cost 25% lower
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base*0.75)
+#            
+#            # advanced nuclear cost 25% higher
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base*1.25,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#        
+#            # advanced nuclear cost 25% lower
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base*0.75,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#        
+#            # different amounts of LDES forced in
+#            for ldes_size_mw in [0,100,1000,10000,50000]:
+#                replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=ldes_size_mw,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#        
+#            # different LDES durations
+#            for ldes_duration in [24,100,200,500]:
+#                replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=ldes_duration,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#        
+#            # no LDES constraints
+#            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=0,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+#
+        if case == "no_vrestor":
             # no zerocarbon CT, still advanced nuclear
             replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-
-            # advanced nuclear cost 25% higher
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base*1.25,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
-            # advanced nuclear cost 25% lower
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base*0.75,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
             # no advanced nuclear (w/ batteries as LDES)
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=0,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=1,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
-            # different amounts of LDES forced in
-            for ldes_size_mw in [0,100,1000,10000,50000]:
-                replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=ldes_size_mw,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
-            # different LDES durations
-            for ldes_duration in [24,100,200,500]:
-                replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=ldes_duration,batteries_as_ldes=0,use_LDES_constraints=1,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
-        
-            # no LDES constraints
-            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=-1,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=0,use_LDES_constraints=0,zerocarbonCTMaxCap=-1,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base)
+            replacements = make_replacements_df(replacements,rep_period_lengths,num_rep_periods,region_to_zone_map,ldes_proportions,advnuclear_cost=advnuclear_cost_base,advnuclear_maxcap=0,ldes_size_mw=1000,ldes_duration=200,batteries_as_ldes=1,use_LDES_constraints=1,zerocarbonCTMaxCap=0,zerocarbonCTCostPerMWYr=zerocarbonCT_cost_base,ZeroCarbonFuelCost=zerocarbon_fuel_cost_base) 
         
         replacements.drop_duplicates(inplace=True)
         replacements["Notes"] = ""
